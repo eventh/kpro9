d11439537d6c2ab6559643bdda7e7757ddddde09
Even <eventh@gmail.com>
2011-09-01 15:54:13 +0200

    test

---
+++ customer/types.h
@@ -0,0 +1,10 @@
+/*
+ * Sample header file for testing Lua C structs script
+ * Copyright 2011, Stig Bjorlykke <stig@bjorlykke.org>
+ */
+
+#ifdef WIN32
+typedef int BOOL;
+#else
+typedef unsigned char BOOL;
+#endif

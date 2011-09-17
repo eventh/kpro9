0412b30cb87fa0fafcd99c16633ff013ee5bb5a0
Even <eventh@gmail.com>
2011-09-17 23:35:11 +0200

    Added another test for cparser.find_structs()

--- source/tests/test_cparser.py
+++ source/tests/test_cparser.py
@@ -14,10 +14,19 @@ if __name__ == '__main__':
 import cparser
 
 
+def _child(node, depth=1):
+    """Find a child in the AST, by depth first."""
+    child = node
+    while depth > 0:
+        child = child.children()[0]
+        depth -= 1
+    return child
+
+# Tests for cparser.parse()
 parse = Tests()
 
 @parse.test
-def basic_types():
+def parse_basic_types():
     """Test that our parser support structs with basic types."""
     ast = cparser.parse('struct simple { int a; float b; char c;};')
     struct = ast.children()[0].children()[0]
@@ -25,12 +34,12 @@ def basic_types():
 
     assert struct.name == 'simple'
     assert a.name == 'a' and b.name == 'b' and c.name == 'c'
-    assert a.children()[0].children()[0].names[0] == 'int'
-    assert b.children()[0].children()[0].names[0] == 'float'
-    assert c.children()[0].children()[0].names[0] == 'char'
+    assert _child(a, 2).names[0] == 'int'
+    assert _child(b, 2).names[0] == 'float'
+    assert _child(c, 2).names[0] == 'char'
 
 @parse.test
-def array_type():
+def parse_array_type():
     """Test arrays as struct members."""
     ast = cparser.parse('struct arr { int a[10]; char b[20]; };')
     struct = ast.children()[0].children()[0]
@@ -38,20 +47,38 @@ def array_type():
 
     assert struct.name == 'arr'
     assert isinstance(a.children()[0], c_ast.ArrayDecl)
-    assert b.children()[0].children()[0].declname == 'b'
-    assert b.children()[0].children()[0].children()[0].names[0] == 'char'
+    assert _child(b, 2).declname == 'b'
+    assert _child(b, 3).names[0] == 'char'
 
 
+# Tests for cparser.find_structs()
 find_structs = Tests()
 
+@find_structs.context
+def create_structs():
+    """Creates an AST and finds all structs within it."""
+    code = '''
+    struct find {
+        int a; float b; char c;
+        char str[30]; float d[3];
+    };
+    '''
+    ast = cparser.parse(code, 'test')
+    yield cparser.find_structs(ast)[0].members
+
 @find_structs.test
-def basic_types():
+def find_basic_types(structs):
     """Test that we find structs which has members of basic types."""
-    ast = cparser.parse('struct simple { int a; float b; char c;};')
-    a, b, c = cparser.find_structs(ast)[0].members
+    a, b, c = structs[:3]
     assert a.name == 'a' and b.name == 'b' and c.name == 'c'
     assert a.type == 'int' and b.type == 'float' and c.type == 'char'
     assert a.size == 4 and b.size == 4 and c.size == 1
+
+@find_structs.test
+def find_array_types(structs):
+    a, b = structs[3:5]
+    assert a.name == 'str' and b.name == 'd'
+    assert a.type == 'char' and b.type == 'float'
 
 
 # Run all tests defined in this module, with PlainReporter

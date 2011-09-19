0cb55fce3fd337cac174b48e0e5c18b1b1a4e6f7
Even <eventh@gmail.com>
2011-09-19 22:30:42 +0200

    Made it more pythonic! :)

--- source/Cli.py
+++ source/Cli.py
@@ -1,43 +1,45 @@
-import argparse
 import sys
+import argparse
+
 
 def main():
+    parser = argparse.ArgumentParser(description='Generate Wireshark'
+            ' dissectors from C structs.')
 
-    parser = argparse.ArgumentParser(description='Needs to parse commands given when the program is started, and supply them to other parts of the program who needs them.')
-    
-    '''C-header file'''
+    # C-header file
     parser.add_argument('-ch', '--cheader', nargs='?',
-                        help='C-header file to parse', metavar='HEADER')
-    
-    '''Configuration file'''
-    parser.add_argument('-c', '--config', action='store', help='Configuration file',
-                        metavar='FILE', dest='configname', nargs='?',
-                        type=argparse.FileType('r'), default=sys.stdin)
-    
-    
-    '''Write output to destination file'''
+            help='C-header file to parse', metavar='HEADER')
+
+    # Configuration file
+    parser.add_argument('-c', '--config', action='store',
+            help='Configuration file', metavar='FILE', dest='configname',
+            nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+
+    # Write output to destination file
     parser.add_argument('-output', nargs='?', help='Write output to file',
-                        metavar='FILE', dest='test.txt', type=argparse.FileType('w'),
-                        default=sys.stdout)
-    
-    
-    '''Verbose flag'''
-    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose',
-                        help='Print information about AST tree, ect.')
-    '''Debug flag'''
-    parser.add_argument('-d', '--debug', action='store_true', dest='debug',
-                        help='Enable debugger')
-    '''No CPP flag'''
-    parser.add_argument('-nocpp', action='store_false', dest='cpp',
-                        help='Disable C preprocessor')
-    
-    '''Set default values'''
+            metavar='FILE', dest='test.txt',
+            type=argparse.FileType('w'), default=sys.stdout)
+
+    # Verbose flag
+    parser.add_argument('-v', '--verbose', action='store_true',
+            dest='verbose', help='Print information about AST tree, ect.')
+
+    # Debug flag
+    parser.add_argument('-d', '--debug', action='store_true',
+            dest='debug', help='Enable debugger')
+
+    # No CPP flag
+    parser.add_argument('-nocpp', action='store_false',
+            dest='cpp', help='Disable C preprocessor')
+
+    # Set default values
     parser.set_defaults(verbose=False, debug=False, cpp=True)
-    
-    
+
+
     args = parser.parse_args()
     print(args)
-    
-    
+
+
 if __name__ == "__main__":
     main()
+

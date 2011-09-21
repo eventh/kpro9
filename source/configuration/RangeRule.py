e4224b150c07c3f03a23d0a5d33fe52e2c77bfb8
Erik Bergersen <eribe@stud.ntnu.no>
2011-09-21 16:17:20 +0200

    Created RangeRule.py and added example struct

---
+++ source/configuration/RangeRule.py
@@ -0,0 +1,34 @@
+import configuration
+
+class ConfigError(Exception):
+    pass
+
+class RangeRule():
+    def __init__(self, file, struct, member, type, minvalue, maxvalue):
+        self.file = file
+        self.struct = struct
+        self.member = member
+        
+    def setType(self, type):
+        if type in Configuration.ValidRangeType:
+            self.type = type
+        else:
+            raise ConfigError()
+
+	def __convert(value):
+		try:
+			value = int(value)
+		except AttributeError:
+			try:
+				value = float(value)
+			except AttributeError
+				raise ConfigError()
+                    
+		return value
+	
+    def setMinvalue(self, minvalue):
+        self.minvalue = __convert(minvalue)
+    
+    def setMaxValue(self, maxvalue):
+        self.maxvalue = __convert(maxvalue)
+            
---
+++ source/headers/example_struct.h
@@ -0,0 +1,6 @@
+#define LENGTH 20
+
+struct example_struct {
+	char name[LENGTH];
+	int percent;
+	};

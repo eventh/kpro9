e72949853e93ba4a220a1abddc97db088aad9f55
Terje Snarby <snarby@stud.ntnu.no>
2011-09-21 22:52:08 +0200

    cli.py

--- source/Cli.py
+++ source/Cli.py
@@ -12,34 +12,31 @@ def Cli():
 def main():
     parser = argparse.ArgumentParser(description='Generate Wireshark'
             ' dissectors from C structs.')
-
-    # C-header file
-    parser.add_argument('-ch', '--cheader', nargs='*',
-            help='C-header file to parse', metavar='HEADER')
-
-    # Configuration file
-    parser.add_argument('-c', '--config', nargs='*',
-            action='store',help='Configuration file')
-
-    # Write output to destination file
-    parser.add_argument('-output', nargs='*', help='Write output to file')
-
+    #Flags listed first:
     # Verbose flag
     parser.add_argument('-v', '--verbose', action='store_true',
             dest='verbose', help='Print information about AST tree, ect.')
-
     # Debug flag
     parser.add_argument('-d', '--debug', action='store_true',
             dest='debug', help='Enable debugger')
-
     # No CPP flag
     parser.add_argument('-nocpp', action='store_false',
             dest='cpp', help='Disable C preprocessor')
-
+    #Followed by input and output files
+    # C-header file
+    parser.add_argument('-ch', '--cheader', nargs='*',
+            type=str,
+            help='C-header file to parse')
+    # Configuration file
+    parser.add_argument('-c', '--config', nargs='*',
+            type=str,
+            action='store',help='Configuration file')
+    # Write output to destination file
+    parser.add_argument('-output', nargs='*',
+            type=str, help='Write output to file')
 
     args = parser.parse_args()
     parser.print_usage()
-
 
 if __name__ == "__main__":
     main()

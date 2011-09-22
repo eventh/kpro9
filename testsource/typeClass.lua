29f83293d3b74f20ca6e44218eb4df2640691582
Sigurd Wien <sigurdwien@gmail.com>
2011-09-22 22:30:38 +0200

    Lua datastructure prototype with flag config for size.

--- testsource/typeClass.lua
+++ testsource/typeClass.lua
@@ -6,27 +6,39 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-print("Hello world!")
-
-Type = {_typename  = "", _default_size = 0, _default_endiam = ""}
+Type = {_typename  = "", _default_size = 0, _default_endiam = "", _flag_configs = {}}
 
 function Type:new (typeName, defualt_size, default_endian)
-  o = {_typeName = typeName, _defualt_size = defualt_size, _default_endian = default_endian}
+  o = {_typeName = typeName, _defualt_size = defualt_size, _default_endian = default_endian, flag_configs = {}}
   setmetatable(o, self)
   self.__index = self
   return o
 end
 
-function Type:add(flag, size, endian) end
+function Type:add(flag, size, endian)
+  self._flag_configs[flag] = {_size = size, _endian = endian}
+end
 
 function Type:getSize(flag)
+  local flag_config = self._flag_configs[flag]
+
+  if not (flag_config == nil) then
+    return flag_config._size
+  end
+
   return self._defualt_size
 end
 
-int = Type:new("int", 4, "big")
+types = {}
 
-int:add(1, 4, "big")
+types["int"] = Type:new("int", 4, "big")
 
-print(int)
+types["char"] = Type:new("char", 4, nil)
 
-print(int:getSize(""))
+types["long"] = Type:new("long", 8, "little")
+
+print(int:getSize(1))
+
+int:add(1, 8, "big")
+
+print(int:getSize(1))

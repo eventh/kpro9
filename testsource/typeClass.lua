e9b57c0abcd175d6c9c560e371eacaf114b3887a
Sigurd Wien <sigurdwien@gmail.com>
2011-09-22 21:34:36 +0200

    Lua datastructure prototype

--- testsource/typeClass.lua
+++ testsource/typeClass.lua
@@ -6,10 +6,27 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-function Type:new(o)
-  o = o or {}
+print("Hello world!")
+
+Type = {_typename  = "", _default_size = 0, _default_endiam = ""}
+
+function Type:new (typeName, defualt_size, default_endian)
+  o = {_typeName = typeName, _defualt_size = defualt_size, _default_endian = default_endian}
   setmetatable(o, self)
-  self.__intex = self
+  self.__index = self
+  return o
 end
 
-function Type:add(a) end
+function Type:add(flag, size, endian) end
+
+function Type:getSize(flag)
+  return self._defualt_size
+end
+
+int = Type:new("int", 4, "big")
+
+int:add(1, 4, "big")
+
+print(int)
+
+print(int:getSize(""))

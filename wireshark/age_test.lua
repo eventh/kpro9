f2dd4ca9b4f4f02835708e845e1d104deb5a4804
Erik Bergersen <eribe@stud.ntnu.no>
2011-09-26 20:11:13 +0200

    Added pcap-file for age_test.h with config

---
+++ wireshark/age_test.lua
@@ -0,0 +1,24 @@
+local proto_age_test = Proto("age_test", "struct age_test")
+local luastructs_dt = DissectorTable.get("luastructs.message")
+
+local f = proto_age_test.fields
+f.name = ProtoField.string("age_test.name", "name")
+f.age = ProtoField.int32("age_test.age", "age")
+
+function proto_age_test.dissector(buffer, pinfo, tree)
+	local subtree = tree:add(proto_age_test, buffer())
+	pinfo.cols.info:append(" (" .. proto_age_test.description .. ")")
+
+	local name_value = subtree:add(f.name,buffer(0,10):string())
+	local age_value = subtree:add(f.age,buffer(10,4):int32())
+	local age_min = 0.0
+	if (age_min > age_value) then
+		age_value:append_text("INVALID")
+	end
+	local age_max = 100.0
+	if (age_max < age_value) then
+		age_value:append_text("INVALID")
+	end
+end
+
+luastructs_dt:add(9, proto_age_test)

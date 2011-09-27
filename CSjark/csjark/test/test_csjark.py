1906120aa267a8e5563ea73517e90d4b661b9df4
Terje Snarby <snarby@stud.ntnu.no>
2011-09-27 14:26:50 +0200

    test_csjark

--- CSjark/csjark/test/test_csjark.py
+++ CSjark/csjark/test/test_csjark.py
@@ -5,11 +5,25 @@ Module for testing the csjark module. Especially command line interface.
 import sys, os
 from attest import Tests, assert_hook
 
+try:
+    import cparser
+except ImportError:
+    # If cparser is not installed, look in parent folder
+    sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), '../'))
+    import cparser
+
+cli_test = Tests()
+
+@cli_test.test
+def cli_test_flag():
+    """Test that the commandline interface flags are working"""
+    assert True
+
 
 
 
 if __name__ == '__main__':
-    all_tests = Tests()
-    all.tests.run()
+    all_tests = Tests([cli_test])
+    all_tests.run()
 
     

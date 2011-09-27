2b3fc3330603e0e2b409019582696e3188896de0
Terje Snarby <snarby@stud.ntnu.no>
2011-09-27 14:04:30 +0200

    created test_csjark, dont know what the other files are :/

---
+++ CSjark/csjark/test/test_csjark.py
@@ -0,0 +1,15 @@
+"""
+Module for testing the csjark module. Especially command line interface.
+"""
+
+import sys, os
+from attest import Tests, assert_hook
+
+
+
+
+if __name__ == '__main__':
+    all_tests = Tests()
+    all.tests.run()
+
+    

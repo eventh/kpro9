0c2e739a6e70c7ec9eec3460ba00fb38ee36ac33
snarby <snarby@stud.ntnu.no>
2011-09-29 09:28:53 +0200

    test_csjark

--- CSjark/csjark/test/test_csjark.py
+++ CSjark/csjark/test/test_csjark.py
@@ -16,19 +16,39 @@ except ImportError:
 cli = Tests()
 
 @cli.test
-def cli_test_flag_verbose():
+def cli_headerfile1():
+    """Test the default commandline interface flags"""
+    headers, configs = csjark.Cli.parse_args(['cpp.h', '-verbose', '-debug'])
+    assert len(headers) == 1
+
+@cli.test
+def cli_headerfile2():
+    """Test the default commandline interface flags"""
+    headers, configs = csjark.Cli.parse_args(['cpp.h', '-verbose', '-debug',
+                                              '-i', 'include.h'])
+    assert len(headers) == 2
+
+@cli.test
+def cli_headerfile_and_configfile():
+    """Test the default commandline interface flags"""
+    headers, configs = csjark.Cli.parse_args(['cpp.h', '-verbose', '-debug',
+                                              '-i', 'include.h', '-config', 'test.yml'])
+    assert len(headers) == 2
+
+@cli.test
+def cli_flag_verbose():
     """Test the default commandline interface flags"""
     csjark.Cli.parse_args(['-verbose', 'cpp.h'])
     assert csjark.Cli.verbose == True
 
 @cli.test
-def cli_test_flag_debug():
+def cli_flag_debug():
     """Test the default commandline interface flags"""
     csjark.Cli.parse_args(['-debug', 'cpp.h'])
     assert csjark.Cli.debug == True
 
 @cli.test
-def cli_test_flag_nocpp():
+def cli_flag_nocpp():
     """Test the default commandline interface flags"""
     csjark.Cli.parse_args(['-nocpp', 'cpp.h'])
     assert csjark.Cli.use_cpp == False

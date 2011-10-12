373318f7882e48f18853213fdfa5cbf5f5b70a17
Even <eventh@gmail.com>
2011-10-12 16:14:19 +0200

    Fixed failing tests. These tests were always wrong, but the test that files actually existed were also wrong. I guess two wrong do make a right.

--- CSjark/csjark/test/test_csjark.py
+++ CSjark/csjark/test/test_csjark.py
@@ -3,55 +3,78 @@ Module for testing the csjark module. Especially command line interface.
 """
 
 import sys, os
-from attest import Tests, assert_hook
+from attest import Tests, assert_hook, contexts
 
 import csjark
 
 
 cli = Tests()
 
+
+@cli.context
+def create_cli():
+    """Create Cli as a context to reset it afterwards."""
+    c = csjark.Cli
+    defaults = c.verbose, c.debug, c.use_cpp, c.output_dir, c.output_file
+    yield c
+    c.verbose, c.debug, c.use_cpp, c.output_dir, c.output_file = defaults
+
 @cli.test
-def cli_headerfile1():
+def cli_headerfile1(cli):
     """Test the default commandline interface flags"""
-    headers, configs = csjark.Cli.parse_args(['cpp.h', '-verbose', '-debug'])
+    header = os.path.join(os.path.dirname(__file__), 'cpp.h')
+    assert os.path.isfile(header)
+    headers, configs = cli.parse_args([header, '-verbose', '-debug'])
     assert len(headers) == 1
 
 #test for requirement FR07A
 @cli.test
-def cli_headerfile2():
+def cli_headerfile2(cli):
     """Test the default commandline interface flags"""
-    headers, configs = csjark.Cli.parse_args(['cpp.h', '-verbose', '-debug',
-                                              '-i', 'include.h'])
+    header = os.path.join(os.path.dirname(__file__), 'cpp.h')
+    include = os.path.join(os.path.dirname(__file__), 'include.h')
+    assert os.path.isfile(include)
+    headers, _ = cli.parse_args([header, '-verbose', '-debug', '-i', include])
     assert len(headers) == 2
 
 #test for requirement FR07B
 @cli.test
-def cli_headerfile_and_configfile():
+def cli_headerfile_and_configfile(cli):
     """Test the default commandline interface flags"""
-    headers, configs = csjark.Cli.parse_args(['cpp.h', '-verbose', '-debug',
-                                              '-i', 'include.h', '-config', 'test.yml'])
+    header = os.path.join(os.path.dirname(__file__), 'cpp.h')
+    include = os.path.join(os.path.dirname(__file__), 'include.h')
+    config = os.path.join(os.path.dirname(__file__), 'test.yml')
+    assert os.path.isfile(config)
+    headers, configs = cli.parse_args(
+            [header, '-verbose', '-debug', '-i', include, '-config', config])
     assert len(headers) == 2
 
 @cli.test
-def cli_flag_verbose():
+def cli_flag_verbose(cli):
     """Test the default commandline interface flags"""
-    csjark.Cli.parse_args(['-verbose', 'cpp.h'])
-    assert csjark.Cli.verbose == True
+    assert cli.verbose == False
+    header = os.path.join(os.path.dirname(__file__), 'cpp.h')
+    cli.parse_args(['-verbose', header])
+    assert cli.verbose == True
 
 @cli.test
-def cli_flag_debug():
+def cli_flag_debug(cli):
     """Test the default commandline interface flags"""
-    csjark.Cli.parse_args(['-debug', 'cpp.h'])
-    assert csjark.Cli.debug == True
+    assert cli.debug == False
+    header = os.path.join(os.path.dirname(__file__), 'cpp.h')
+    cli.parse_args(['-debug', header])
+    assert cli.debug == True
 
 @cli.test
-def cli_flag_nocpp():
+def cli_flag_nocpp(cli):
     """Test the default commandline interface flags"""
-    csjark.Cli.parse_args(['-nocpp', 'cpp.h'])
-    assert csjark.Cli.use_cpp == False
+    assert cli.use_cpp == True
+    header = os.path.join(os.path.dirname(__file__), 'cpp.h')
+    cli.parse_args(['-nocpp', header])
+    assert cli.use_cpp == False
 
 @cli.test
-def cli_file_dont_existing():
+def cli_file_dont_existing(cli):
     """Test if a file is missing"""
-    headers, configs = csjark.Cli.parse_args(['filedontexsist.h', 'test.yml'])
-    assert ''
+    with contexts.raises(SystemExit) as error:
+        headers, configs = cli.parse_args(['filedontexsist.h', 'test.yml'])

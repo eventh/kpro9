d9038ba052a25bd329bda4455fce8ca8600b7b8b
Metareven <larssot@stud.ntnu.no>
2011-10-17 10:26:16 +0200

    testing part 2

---
+++ CSjark/csjark/headers/black_box_s2.h
@@ -0,0 +1,17 @@
+#include struct_test
+enum gender{male,female = 0, alien, none};
+
+struct its_just_a_model{
+
+//enumtest
+enum gender gend;
+/arraytests
+char chararr[3][3]
+int intarr[2]
+/bitstringtests
+int flags;
+short color;
+//struct and struct within struct test
+struct struct_test black_knight;
+
+}

3aa63d4408114f0fe95bce825fca90ad4ebf3b2d
Even <eventh@gmail.com>
2011-11-06 22:33:46 +0100

    Something.

--- CSjark/csjark/cpp.py
+++ CSjark/csjark/cpp.py
@@ -29,12 +29,6 @@ def parse_file(filename, platform=None, includes=None):
     if os.path.dirname(filename):
         path_list.append('-I%s' % os.path.dirname(filename))
 
-    # Add arguments to #include if 'includes' is given
-    #if includes is not None:
-    #    print("wtf", ['-i "%s"' % i for i in includes])
-    #    path_list.extend(['-include%s' % i for i in includes])
-    # TODO: cpp.exe dont accept -include,wtf!
-
     # Define macros
     if platform is not None:
         #path_list.append('-undef') # Remove system-specific defines
@@ -43,10 +37,20 @@ def parse_file(filename, platform=None, includes=None):
 
     # Add any C preprocesser arguments from CLI or config
     path_list.extend('-I%s' % i for i in Options.cpp_include_dirs)
-    path_list.extend('-include "%s"' % i for i in Options.cpp_includes)
+    path_list.extend('-include%s' % i for i in Options.cpp_includes)
     path_list.extend('-D%s' % i for i in Options.cpp_defines)
     path_list.extend('-U%s' % i for i in Options.cpp_undefines)
     path_list.extend(Options.cpp_args)
+
+    # Add arguments to #include if 'includes' is given
+    #if includes is not None:
+    #    if sys.platform == 'win32':
+    #        inc = list(includes) + [filename]
+    #        filename = 'tmpfile.h'
+    #        with open(filename, 'w') as f:
+    #            f.write('\n'.join('#include "%s"' % i for i in inc) + '\n')
+    #    else:
+    #        path_list.extend(['-include%s' % i for i in includes])
 
     # Call C preprocessor with args and file
     path_list.append(filename)
@@ -70,34 +74,6 @@ def post_cpp(lines):
 def pre_cpp(lines):
     """Perform a pre preprocessing step."""
     return lines
-
-
-'''Not needed yet.
-def pre_cpp_files(files):
-    """Modify all 'files' to ensure we can parse them."""
-    # Create directory for holding the new files
-    tmp_folder = 'csjark_tmp_files'
-    if not os.path.exists(tmp_folder):
-        os.makedirs(tmp_folder, exist_ok=True)
-    if not os.path.isdir(tmp_folder):
-        raise IOError('Failed to create tmp folder %s' % tmp_folder)
-
-    # Go though one file at a time and rewrite them
-    mapping = {}
-    for filename in files:
-        lines = pre_process(filename)
-
-        # Write the new lines in a file
-        new_file = os.path.join(tmp_folder, filename)
-        os.makedirs(os.path.dirname(new_file), exist_ok=True)
-        with open(new_file, 'w') as f:
-            f.write(''.join(lines))
-
-        mapping[new_file] = filename
-
-    # Return new filenames and mapping of new to old filename
-    return list(mapping.keys()), mapping
-'''
 
 
 def _get_cpp():

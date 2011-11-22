a9cf76a95e69e8f1fcfdbc3142c8e6dd000db4ab
Erik Bergersen <eribe@stud.ntnu.no>
2011-11-23 00:38:21 +0100

    sprint 2 fixing

--- report/sprints/code/typedef.h
+++ report/sprints/code/typedef.h
@@ -1,6 +1,7 @@
 typedef int BOOL;
 
-typedef enum traffic_light_t { red, yellow, green } traffic_light_t;
+typedef enum traffic_light_t { red, yellow, green }
+	traffic_light_t;
 
 typedef struct name_t {
 	char first_name[20];
